3eb6c0f184b7d276a4139cb31f545060366d25ad
Morven Lewis-Everley <morven@i-lateral.com>
2023-10-19 17:36:34 +0100

    Add sorting to customisation lists

--- code/model/ProductCustomisationList.php
+++ code/model/ProductCustomisationList.php
@@ -14,6 +14,46 @@ class ProductCustomisationList extends DataObject
         "Customisations" => "ProductCustomisation"
     );
 
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName('Customisations');
+
+        // Only add fields if the object exists
+        if ($this->ID) {
+            // Deal with customisations
+            $add_button = new GridFieldAddNewButton('toolbar-header-left');
+            $add_button->setButtonName(_t(
+                "CustomisableProduct.AddCustomisation",
+                "Add Customisation"
+            ));
+
+            $custom_config = GridFieldConfig::create()->addComponents(
+                new GridFieldToolbarHeader(),
+                $add_button,
+                new GridFieldSortableHeader(),
+                new GridFieldDataColumns(),
+                new GridFieldPaginator(20),
+                new GridFieldEditButton(),
+                new GridFieldDeleteAction(),
+                new GridFieldDetailForm(),
+                new GridFieldOrderableRows('Sort')
+            );
+
+            $fields->addFieldToTab(
+                'Root.Main',
+                GridField::create(
+                    'Customisations',
+                    '',
+                    $this->Customisations(),
+                    $custom_config
+                )
+            );
+        }
+
+        return $fields;
+    }
+
     public function onBeforeDelete() {
         parent::onBeforeDelete();
 
